35c1b25ac598256f1b6499a6ef82e256666f5550
Melanie Murray <melaniemurr13@gmail.com>
2026-08-09 22:02:26 -0600

    Implement navigation and base UI structure

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,26 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
+import { tokens } from '@/theme/tokens';
+
 export default function RootLayout() {
   return (
     <>
-      <Stack screenOptions={{ headerShown: false }} />
+      <Stack
+        screenOptions={{
+          contentStyle: { backgroundColor: tokens.color.background },
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen
+          name="check-in"
+          options={{
+            animation: 'slide_from_bottom',
+            presentation: 'modal',
+          }}
+        />
+      </Stack>
       <StatusBar style="dark" />
     </>
   );
